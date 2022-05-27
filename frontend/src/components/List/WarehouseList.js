bf4646677b125cbdec4f5484c09075019196b3d6
choyeongwoo <choyeongwoo.s@gmail.com>
2022-05-27 16:20:47 +0900

    warehouse list 데이터바인딩

--- frontend/src/components/List/WarehouseList.js
+++ frontend/src/components/List/WarehouseList.js
@@ -8,19 +8,52 @@ function WarehouseList() {
   const [warehouses, setwarehouses] = useState([])
   const [search, setsearch] = useState(false)
   // 데이터바인딩
-  const [location, setlocation] = useState("all")
-  const [inventory_using, setinventory_using] = useState("all")
-  const [purpose, setPurpose] = useState("all")
-  const [use, setUse] = useState("all")
+  const [location, setLocation] = useState(null)
+  const [warehouse_code, setWarehouse_code] = useState(null)
+  const [purpose, setPurpose] = useState(null)
+  const [warehouse_code_desc, setWarehouse_code_desc] = useState(null)
+  const [use, setUse] = useState(null)
+  const [minimum_weight, setMinimum_weight] = useState(null)
+  const [maximum_weight, setMaximum_weight] = useState(null)
+  const [minumum_count, setMinumum_count] = useState(null)
+  const [maximum_count, setMaximum_count] = useState(null)
+  const [inventory_using, setInventory_using] = useState(null)
+  const [remarks, setRemarks] = useState(null)
   //
   useEffect(() => {
     // 창고조회
     axios.defaults.baseURL = "http://192.168.0.10:8081"
-    axios.get('/warehouse', {})
+    axios.get('/warehouse/search', {
+      location: location,
+      warehouse_code: warehouse_code,
+      purpose: purpose,
+      use: use,
+      minimum_weight: minimum_weight,
+      maximum_weight: maximum_weight,
+      minumum_count: minumum_count,
+      maximum_count: maximum_count,
+      inventory_using: inventory_using,
+    })
       .then((res) => { setwarehouses(res.data) })
       .catch((err) => { console.log(err) })
   }, [search])
 
+  function searchAll() {
+    axios.defaults.baseURL = "http://192.168.0.10:8081"
+    axios.get('/warehouse', {
+      location: location,
+      warehouse_code: warehouse_code,
+      purpose: purpose,
+      use: use,
+      minimum_weight: minimum_weight,
+      maximum_weight: maximum_weight,
+      minumum_count: minumum_count,
+      maximum_count: maximum_count,
+      inventory_using: inventory_using,
+    })
+      .then((res) => { setwarehouses(res.data) })
+      .catch((err) => { console.log(err) })
+  }
   return (
     <div className="max-w-screen-2xl mx-auto my-10">
       <div className="font-bold text-2xl text-center my-10">창고조회</div>
@@ -39,11 +72,12 @@ function WarehouseList() {
                   name="dropdown"
                   autoComplete="dropdown-name"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  onClick={(e) => { setLocation(e.target.value) }}
                 >
-                  <option value="">전체</option>
-                  <option value="">천안</option>
-                  <option value="">광양</option>
-                  <option value="">포항</option>
+                  <option value="all">전체</option>
+                  <option value="천안">천안</option>
+                  <option value="광양">광양</option>
+                  <option value="포항">포항</option>
                 </select>
               </div>
               <div className="col-span-1">
@@ -55,11 +89,12 @@ function WarehouseList() {
                   name="inventory_using"
                   autoComplete="inventory_using-name"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  onClick={(e) => { setInventory_using(e.target.value) }}
                 >
                   <option value="all">전체</option>
-                  <option value="gudong">구동</option>
-                  <option value="jeonjang">전장</option>
-                  <option value="etc">기타</option>
+                  <option value="구동">구동</option>
+                  <option value="전장">전장</option>
+                  <option value="기타">기타</option>
                 </select>
               </div>
               <div className="col-span-1">
@@ -71,11 +106,12 @@ function WarehouseList() {
                   name="dropdown"
                   autoComplete="dropdown-name"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  onClick={(e) => { setPurpose(e.target.value) }}
                 >
-                  <option value="">전체</option>
-                  <option value="">완제품</option>
-                  <option value="">반제품</option>
-                  <option value="">불량품</option>
+                  <option value="all">전체</option>
+                  <option value="완제품">완제품</option>
+                  <option value="반제품">반제품</option>
+                  <option value="불량품">불량품</option>
                 </select>
               </div>
 
@@ -88,10 +124,11 @@ function WarehouseList() {
                   name="dropdown"
                   autoComplete="dropdown-name"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  onClick={(e) => { setUse(e.target.value) }}
                 >
-                  <option value="">전체</option>
-                  <option value="">YES</option>
-                  <option value="">NO</option>
+                  <option value="all">전체</option>
+                  <option value="사용">사용</option>
+                  <option value="미사용">미사용</option>
 
                 </select>
               </div>
@@ -104,17 +141,18 @@ function WarehouseList() {
                   name="dropdown"
                   autoComplete="dropdown-name"
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-sky-500 focus:border-sky-500 sm:text-sm"
+                  onClick={(e) => { setWarehouse_code(e.target.value) }}
                 >
-                  <option value="">전체</option>
-                  <option value="">A1</option>
-                  <option value="">A2</option>
-                  <option value="">A3</option>
-                  <option value="">B1</option>
-                  <option value="">B2</option>
-                  <option value="">B3</option>
-                  <option value="">C1</option>
-                  <option value="">C2</option>
-                  <option value="">C3</option>
+                  <option value="all">전체</option>
+                  <option value="A1">A1</option>
+                  <option value="A2">A2</option>
+                  <option value="A3">A3</option>
+                  <option value="B1">B1</option>
+                  <option value="B2">B2</option>
+                  <option value="B3">B3</option>
+                  <option value="C1">C1</option>
+                  <option value="C2">C2</option>
+                  <option value="C3">C3</option>
                 </select>
               </div>
 
@@ -123,20 +161,24 @@ function WarehouseList() {
                 <div className=''>weight</div>
                 <div>
                   <input
-                    type="text"
+                    type="number"
+                    min={0}
                     name="text"
                     id="text"
                     autoComplete="address-level2"
                     className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    onChange={(e) => { setMinimum_weight(e.target.value) }}
                   /></div>
                 <div className='text-xs'>-</div>
                 <div>
                   <input
-                    type="text"
+                    type="number"
+                    min={0}
                     name="text"
                     id="text"
                     autoComplete="address-level2"
                     className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    onChange={(e) => { setMaximum_weight(e.target.value) }}
                   />
                 </div>
               </div>
@@ -144,74 +186,27 @@ function WarehouseList() {
                 <div className=''>count</div>
                 <div>
                   <input
-                    type="text"
+                    type="number"
+                    min={0}
                     name="text"
                     id="text"
                     autoComplete="address-level2"
                     className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    onChange={(e) => { setMinumum_count(e.target.value) }}
                   /></div>
                 <div className='text-xs'>-</div>
                 <div>
                   <input
-                    type="text"
+                    type="number"
+                    min={0}
                     name="text"
                     id="text"
                     autoComplete="address-level2"
                     className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    onChange={(e) => { setMaximum_count(e.target.value) }}
                   />
                 </div>
               </div>
-              <div className="col-span-1 grid grid-cols-4 text-center">
-                <div className=''>높이</div>
-                <div>
-                  <input
-                    type="text"
-                    name="text"
-                    id="text"
-                    autoComplete="address-level2"
-                    className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  /></div>
-                <div className='text-xs'>-</div>
-                <div>
-                  <input
-                    type="text"
-                    name="text"
-                    id="text"
-                    autoComplete="address-level2"
-                    className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-              </div>
-              {/* 수량 */}
-              <div className="col-span-1">
-                <div className='grid grid-cols-3'>
-                  <div>수량</div>
-                  <div className='col-span-2'><input
-                    type="text"
-                    name="text"
-                    id="text"
-                    autoComplete="address-level2"
-                    className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  /></div>
-                </div>
-              </div>
-              {/*  */}
-              {/* 중량 */}
-              <div className="col-span-1">
-                <div className='grid grid-cols-3'>
-                  <div>중량</div>
-                  <div className='col-span-2'><input
-                    type="text"
-                    name="text"
-                    id="text"
-                    autoComplete="address-level2"
-                    className="mt-1 focus:ring-sky-500 focus:border-sky-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  /></div>
-                </div>
-              </div>
-              {/*  */}
-
-
             </div>
           </div>
           <div className="px-4 py-3 text-right">
