74dbaa3310ba824bc72fc2a0a5dbe10ea5e6ed86
wjddkswl97 <wjddkswl97@gmail.com>
2022-06-20 10:41:43 +0900

    mypage

--- frontend/src/components/Account/Mypage.js
+++ frontend/src/components/Account/Mypage.js
@@ -3,23 +3,21 @@ import Timeline from "../Mypage/Timeline";
 import { Card } from "antd";
 import MyDescription from "../Mypage/MyDescription";
 const { Meta } = Card;
+
 function Mypage() {
   return (
-    <div>
-      <div className="grid grid-cols-4 gap-1 text-center ">
-       
-          {/* <img className="rounded-lg w-60 h-60"
+    <div className="w-2/3 mx-auto">
+      <div className="grid grid-cols-4 gap-1 text-center">
+        <div className="mx-auto">
+          <img
+            className="rounded-lg w-60 h-60"
             alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          /> */}
-  
+            src="./Images/mypage.png"
+          />
+        </div>
         <div className="col-span-3 mx-auto">
           <MyDescription />
         </div>
-        {/* <div className="grid grid-cols-2 grid-rows-2 gap-4 text-center">
-          <div className='col-span-3 mx-auto'><MyDescription /></div>
-          <div className='col-span-2 mx-auto'>Your Dashboard</div>
-     </div> */}
       </div>
     </div>
   );
