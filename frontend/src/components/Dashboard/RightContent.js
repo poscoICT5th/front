5401dcbed331cd153156387f2fee288c831be54b
choyeongwoo <choyeongwoo.s@gmail.com>
2022-06-27 17:44:46 +0900

    rightcontent 수정중

--- frontend/src/components/Dashboard/RightContent.js
+++ frontend/src/components/Dashboard/RightContent.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import axios from "axios"
 function RightContent() {
+   useEffect(() => {
+     axios.get()
+   }, [])
    
     return (
         <div className="col-span-1">
@@ -9,25 +12,25 @@ function RightContent() {
                 <div className='bg-cyan-100 rounded-lg h-20'>
                     <div className="grid grid-cols-2 h-full">
                         <div className='my-auto mx-auto'>처리한것</div>
-                        <div className="my-auto mx-auto">처리할것</div>
+                        <div className="my-auto mx-auto">10</div>
                     </div>
                 </div>
                 <div className='bg-cyan-100 rounded-lg h-20'>
                     <div className="grid grid-cols-2 h-full">
                         <div className='my-auto mx-auto'>처리한것</div>
-                        <div className="my-auto mx-auto">처리할것</div>
+                        <div className="my-auto mx-auto">10</div>
                     </div>
                 </div>
                 <div className='bg-cyan-100 rounded-lg h-20'>
                     <div className="grid grid-cols-2 h-full">
                         <div className='my-auto mx-auto'>처리한것</div>
-                        <div className="my-auto mx-auto">처리할것</div>
+                        <div className="my-auto mx-auto">10</div>
                     </div>
                 </div>
                 <div className='bg-cyan-100 rounded-lg h-20'>
                     <div className="grid grid-cols-2 h-full">
                         <div className='my-auto mx-auto'>처리한것</div>
-                        <div className="my-auto mx-auto">처리할것</div>
+                        <div className="my-auto mx-auto">10</div>
                     </div>
                 </div>
             </div>
