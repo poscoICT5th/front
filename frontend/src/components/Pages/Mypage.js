9d54cd210cc43b86928efd32c5ffb3856ad33169
wjddkswl97 <wjddkswl97@gmail.com>
2022-07-15 15:22:10 +0900

    트렌드 없음 처리

--- frontend/src/components/Pages/Mypage.js
+++ frontend/src/components/Pages/Mypage.js
@@ -1,69 +1,97 @@
 import React, { useEffect, useState } from "react";
-import jwtDecode from 'jwt-decode';
+import jwtDecode from "jwt-decode";
+import { Divider } from "antd";
 
 function Mypage(props) {
   const [userName, setUserName] = useState("");
   const [phone, setPhone] = useState(null);
   const [email, setEmail] = useState("");
   const [team, setTeam] = useState("");
-  const proverbs = ["Believe in yourself.", "Life is a journey",
-    "Don't dream, Be it", "No sweat, No sweet", "No pain, No gain", "Be brave"]
+  const proverbs = [
+    "Believe in yourself.",
+    "Life is a journey",
+    "Don't dream, Be it",
+    "No sweat, No sweet",
+    "No pain, No gain",
+    "Be brave",
+  ];
   const getRandom = function (length) {
     return proverbs[parseInt(Math.random() * length)];
-  }
+  };
 
   useEffect(() => {
     if (true) {
-      setUserName(jwtDecode(sessionStorage.getItem('token')).info.userName);
-      setPhone(jwtDecode(sessionStorage.getItem('token')).info.phone);
-      setEmail(jwtDecode(sessionStorage.getItem('token')).info.email);
-      setTeam(jwtDecode(sessionStorage.getItem('token')).info.team);
+      setUserName(jwtDecode(sessionStorage.getItem("token")).info.userName);
+      setPhone(jwtDecode(sessionStorage.getItem("token")).info.phone);
+      setEmail(jwtDecode(sessionStorage.getItem("token")).info.email);
+      setTeam(jwtDecode(sessionStorage.getItem("token")).info.team);
     }
-  }, [userName])
+  }, [userName]);
+  //날짜
+  const date = new Date();
+  const day = date.getDay();
+  const month = date.getMonth();
+  const datedate = date.getDate();
+  var week = new Array(
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일"
+  );
+  var today = week[day];
+
   return (
-    <div className="w-2/3 mx-auto" data-aos="fade-up">
-      <div className="grid grid-cols-1 gap-1 text-center">
-        <div className="col-span-3 mx-auto">
-          <div className="shadow overflow-hidden sm:rounded-lg">
-            <div className="px-4 py-5 sm:px-6">
-              <h3 className="text-lg leading-6 font-medium">
-                회원 정보
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">오늘도 화이팅~</p>
-            </div>
-            <div className="border-t border-gray-200">
-              <dl>
-                <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Name</dt>
-                  <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+    <div>
+      <header className="">
+        <div className="mx-auto py-2 px-5">
+          <h1 className="text-4xl font-bold">마이페이지</h1>
+        </div>
+      </header>
+      <Divider orientation="left">
+        {month + 1}월 {datedate}일 {today} 오늘도 화이팅
+      </Divider>
+      <div className="w-1/2 mx-auto mt-20" data-aos="fade-up">
+        <div className="text-center">
+          <div className="mx-auto">
+            <div className="shadow overflow-hidden sm:rounded-lg">
+              <div className="border-t border-gray-300">
+                <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 mt-1">
+                  <dt className="text-xl font-bold ">이름</dt>
+                  <dd className="text-xl sm:mt-0 sm:col-span-2 text-gray-500">
                     {userName}
                   </dd>
                 </div>
+
                 <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Team</dt>
-                  <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  <dt className="text-xl font-medium font-bold">담당창고</dt>
+                  <dd className="mt-1 text-xl sm:mt-0 sm:col-span-2 text-gray-500">
                     {team}
                   </dd>
                 </div>
                 <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Phone</dt>
-                  <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  <dt className="text-xl font-medium font-bold">휴대폰</dt>
+                  <dd className="mt-1 text-xl sm:mt-0 sm:col-span-2 text-gray-500">
                     {phone}
                   </dd>
                 </div>
                 <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Email Address</dt>
-                  <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  <dt className="text-xl font-bold">이메일</dt>
+                  <dd className="mt-1 text-xl sm:mt-0 sm:col-span-2 text-gray-500">
                     {email}
                   </dd>
                 </div>
                 <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">Thought Of The Day</dt>
-                  <dd className="mt-1 text-sm sm:mt-0 sm:col-span-2">
+                  <dt className="text-xl font-medium font-bold">
+                    오늘의한마디
+                  </dt>
+                  <dd className="mt-1 text-xl sm:mt-0 sm:col-span-2 text-gray-500">
                     {getRandom(proverbs.length)}
                   </dd>
                 </div>
-              </dl>
+              </div>
             </div>
           </div>
         </div>
--- frontend/src/components/Pages/TrendInventory.js
+++ frontend/src/components/Pages/TrendInventory.js
@@ -10,24 +10,26 @@ function TrendInventory() {
   let inventoryURL = useSelector((state) => state.inventoryURL);
   const [clickData, setClickData] = useState({
     month: 1,
-  })
-  const years = [2022, 2021, 2020]
-  const months =
-  {
-    "Jan": "01",
-    "Feb": "02",
-    "Mar": "03",
-    "Apr": "04",
-    "May": "05",
-    "Jun": "06",
-    "Jul": "07",
-    "Aug": "08",
-    "Sep": "09",
-    "Oct": "10",
-    "Nov": "11",
-    "Dec": "12"
-  }
+  });
+  const years = [2022, 2021, 2020];
+  const months = {
+    Jan: "01",
+    Feb: "02",
+    Mar: "03",
+    Apr: "04",
+    May: "05",
+    Jun: "06",
+    Jul: "07",
+    Aug: "08",
+    Sep: "09",
+    Oct: "10",
+    Nov: "11",
+    Dec: "12",
+  };
   const [datas, setDatas] = useState({
+    day_2022: 0,
+    day_2021: 0,
+    day_2020: 0,
     sum_export_motor_2022: 0,
     sum_export_motor_2021: 0,
     sum_export_motor_2020: 0,
@@ -45,46 +47,51 @@ function TrendInventory() {
     sum_inven_rotor_2020: 0,
     sum_inven_strip_2022: 0,
     sum_inven_strip_2021: 0,
-    sum_inven_strip_2020: 0
-  })
+    sum_inven_strip_2020: 0,
+  });
 
   const { TabPane } = Tabs;
   function getTrendDatas(year) {
     axios.defaults.baseURL = inventoryURL;
-    axios.get(`/trend/year/${year}/month/${clickData.month}`)
-      .then((res) => {
-        setDatas({
-          ...datas,
-          [`sum_export_motor_${year}`]: res.data[0]['sum_export_motor'],
-          [`sum_export_rotor_${year}`]: res.data[0]['sum_export_rotor'],
-          [`sum_export_strip_${year}`]: res.data[0]['sum_export_strip'],
-          [`sum_inven_motor_${year}`]: res.data[0]['sum_inven_motor'],
-          [`sum_inven_rotor_${year}`]: res.data[0]['sum_inven_rotor'],
-          [`sum_inven_strip_${year}`]: res.data[0]['sum_inven_strip'],
-        })
-      })
+    axios.get(`/trend/year/${year}/month/${clickData.month}`).then((res) => {
+      console.log(res.data[0]["day"]);
+      setDatas({
+        ...datas,
+        [`sum_export_motor_${year}`]: res.data[0]["sum_export_motor"],
+        [`sum_export_rotor_${year}`]: res.data[0]["sum_export_rotor"],
+        [`sum_export_strip_${year}`]: res.data[0]["sum_export_strip"],
+        [`sum_inven_motor_${year}`]: res.data[0]["sum_inven_motor"],
+        [`sum_inven_rotor_${year}`]: res.data[0]["sum_inven_rotor"],
+        [`sum_inven_strip_${year}`]: res.data[0]["sum_inven_strip"],
+        [`day_${year}`]: res.data[0]["sum_inven_strip"],
+      });
+    });
   }
   useEffect(() => {
-    years.forEach(year => {
-      getTrendDatas(year)
+    years.forEach((year) => {
+      getTrendDatas(year);
     });
-  }, [clickData])
+  }, [clickData]);
 
   return (
     <div data-aos="fade-up" className="w-3/4 mx-auto">
       <div className="">
-        <div className="font-bold text-2xl text-center mb-5">Inventory Trend</div>
+        <div className="font-bold text-2xl text-center mb-5">
+          Inventory Trend
+        </div>
         <div className="grid grid-cols-12 gap-2">
-          {
-            Object.keys(months).map((value) => {
-              return <button
+          {Object.keys(months).map((value) => {
+            return (
+              <button
                 className="py-2 font-bold text-sm rounded-lg text-gray-700 hover:text-white bg-sky-100 hover:bg-sky-700"
-                onClick={() => { setClickData({ ...clickData, month: months[value] }) }}
+                onClick={() => {
+                  setClickData({ ...clickData, month: months[value] });
+                }}
               >
                 {value}
               </button>
-            })
-          }
+            );
+          })}
         </div>
         {/* table */}
         <div className="flex flex-col mx-1 mt-5 text-center max-h-72">
@@ -121,17 +128,35 @@ function TrendInventory() {
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200">
-                    {
-                      years.map((year) => {
-                        return <>
+                    {years.map((year) => {
+                      return (
+                        <>
                           <tr>
                             <td className="px-6 py-4 whitespace-nowrap">
                               <div className="text-sm">{year}</div>
                             </td>
                             <td className="px-6 py-4 whitespace-nowrap">
                               <div className="text-sm">
+                                {datas[`sum_export_motor_${year}`] *
+                                  datas[`sum_inven_motor_${year}`] !==
+                                0 ? (
+                                  (
+                                    (datas[`sum_export_motor_${year}`] /
+                                      datas[`sum_inven_motor_${year}`] /
+                                      datas[`day_${year}`]) *
+                                    100
+                                  ).toFixed(2)+"%"
+                                ) : (
+                                  <div>-</div>
+                                )}
+                              </div>
+                            </td>
+                            <td className="px-6 py-4 whitespace-nowrap">
+                              <div className="text-sm">
                                 {(
-                                  (datas.sum_export_motor_ + year / datas.sum_inven_motor_ + year / 178) *
+                                  (datas[`sum_export_rotor_${year}`] /
+                                    datas[`sum_inven_rotor_${year}`] /
+                                    datas[`day_${year}`]) *
                                   100
                                 ).toFixed(2)}
                                 %
@@ -140,16 +165,9 @@ function TrendInventory() {
                             <td className="px-6 py-4 whitespace-nowrap">
                               <div className="text-sm">
                                 {(
-                                  (datas.sum_export_rotor_ + year / datas.sum_inven_rotor_ + year / 178) *
-                                  100
-                                ).toFixed(2)}
-                                %
-                              </div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap">
-                              <div className="text-sm">
-                                {(
-                                  (datas.sum_export_strip_ + year / datas.sum_inven_strip_ + year / 178) *
+                                  (datas[`sum_export_strip_${year}`] /
+                                    datas[`sum_inven_strip_${year}`] /
+                                    datas[`day_${year}`]) *
                                   100
                                 ).toFixed(2)}
                                 %
@@ -157,8 +175,8 @@ function TrendInventory() {
                             </td>
                           </tr>
                         </>
-                      })
-                    }
+                      );
+                    })}
                   </tbody>
                 </table>
               </div>
